381234789ef8d6ccc8a01d1ed243f0dc06ed3a2c
Manikandan Sundararajan <mani@hey.com>
2021-12-21 12:55:58 -0500

    top down view of a simple network
    Implement the propagate fn for Network using the folding pattern

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,39 @@
+pub struct Network {
+    // a network is built from layers
+    layers: Vec<Layer>,
+}
+
+struct Layer {
+    // a layer is built from neurons
+    neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    // a neuron contains biases and output weights
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        // for layer in &self.layers {
+        //     inputs = layer.propagate(inputs);
+        // }
+
+        // use the folding pattern
+
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
