bc1933c71124756e2bdca1393347776d43dc62ad
Manikandan Sundararajan <mani@hey.com>
2021-12-22 14:52:41 -0500

    impl IntoIterator for Chromosome using some naughty features

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(min_type_alias_impl_trait)]
+
+use std::iter::FromIterator;
 use std::ops::Index;
 
 use rand::seq::SliceRandom;
@@ -77,11 +80,26 @@ impl Index<usize> for Chromosome {
     }
 }
 
+// ---
+// | this is the "type of iterator" for which you want to provide
+// | `from_iter()` and `collect()`
+// |
+// | sometimes it's called the type the iterator *yields*
+// -------------- v-v
 impl FromIterator<f32> for Chromosome {
     fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Self {
         Self {
             genes: iter.into_iter().collect(),
         }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = impl Iterator<Item = f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
     }
 }
 
