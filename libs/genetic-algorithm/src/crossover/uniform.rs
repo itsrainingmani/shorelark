1a51994a2d172b813423f8cc46c9a3641d7ef331
Manikandan Sundararajan <mani@hey.com>
2021-12-22 18:22:00 -0500

    create separate crossover and method file

---
+++ libs/genetic-algorithm/src/crossover/uniform.rs
@@ -0,0 +1,28 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct UniformCrossover;
+
+impl UniformCrossover {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl CrossoverMethod for UniformCrossover {
+    fn crossover(
+        &self,
+        rng: &mut dyn RngCore,
+        parent_a: &Chromosome,
+        parent_b: &Chromosome,
+    ) -> Chromosome {
+        assert_eq!(parent_a.len(), parent_b.len());
+        let parent_a = parent_a.iter();
+        let parent_b = parent_b.iter();
+
+        parent_a
+            .zip(parent_b)
+            .map(|(&a, &b)| if rng.gen_bool(0.5) { a } else { b })
+            .collect()
+    }
+}
