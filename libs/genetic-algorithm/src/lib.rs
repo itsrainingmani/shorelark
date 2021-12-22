eb61e5e6942abff687368f811481033cf6df9087
Manikandan Sundararajan <mani@hey.com>
2021-12-21 23:21:29 -0500

    scaffold an evolve function

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -4,4 +4,19 @@ impl GeneticAlgorithm {
     pub fn new() -> Self {
         Self
     }
+
+    // we want this to be generic, so we use a type paramter
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+        // precondition
+        assert!(!population.is_empty());
+
+        (0..population.len())
+            .map(|_| {
+                // TODO selection
+                // TODO crossover
+                // TODO mutation
+                todo!()
+            })
+            .collect()
+    }
 }
