0268a98281ab0af18be89101d546a548a6236cc7
Manikandan Sundararajan <mani@hey.com>
2021-12-22 18:22:17 -0500

    use the crossover and chromosome implementation

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -2,7 +2,7 @@
 #![feature(min_type_alias_impl_trait)]
 
 pub use self::{
-    individual::*, selection::*, chromosome::*
+    individual::*, selection::*, chromosome::*, crossover::*
 };
 
 use rand::seq::SliceRandom;
@@ -11,45 +11,10 @@ use rand::{Rng, RngCore};
 mod individual;
 mod selection;
 mod chromosome;
+mod crossover;
 
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
-}
-
-pub trait CrossoverMethod {
-    fn crossover(
-        &self,
-        rng: &mut dyn RngCore,
-        parent_a: &Chromosome,
-        parent_b: &Chromosome,
-    ) -> Chromosome;
-}
-
-#[derive(Clone, Debug)]
-pub struct UniformCrossover;
-
-impl UniformCrossover {
-    pub fn new() -> Self {
-        Self
-    }
-}
-
-impl CrossoverMethod for UniformCrossover {
-    fn crossover(
-        &self,
-        rng: &mut dyn RngCore,
-        parent_a: &Chromosome,
-        parent_b: &Chromosome,
-    ) -> Chromosome {
-        assert_eq!(parent_a.len(), parent_b.len());
-        let parent_a = parent_a.iter();
-        let parent_b = parent_b.iter();
-
-        parent_a
-            .zip(parent_b)
-            .map(|(&a, &b)| if rng.gen_bool(0.5) {a} else {b})
-            .collect()
-    }
 }
 
 impl<S> GeneticAlgorithm<S>
