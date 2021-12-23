81fbe616d8265373444e70db4f02aefd717eb00f
Manikandan Sundararajan <mani@hey.com>
2021-12-23 07:00:37 -0500

    start displaying animals within a canvas

--- www/index.js
+++ www/index.js
@@ -1,6 +1,23 @@
 import * as sim from 'lib-simulation-wasm';
 
 const simulation = new sim.Simulation();
-const world = simulation.world();
+const viewport = document.getElementById('viewport');
+const viewportWidth = viewport.width;
+const viewportHeight = viewport.height;
 
-console.log(world);
+const viewportScale = window.devicePixelRatio || 1;
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
+const ctxt = viewport.getContext('2d');
+
+ctxt.scale(viewportScale, viewportScale);
+
+ctxt.fillStyle = 'rgb(0, 0, 0)';
+
+for (const animal of simulation.world().animals) {
+	ctxt.fillRect(animal.x * viewportWidth, animal.y * viewportHeight, 15, 15);
+}
