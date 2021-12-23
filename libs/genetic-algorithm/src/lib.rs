56eeb4e52ea4874052764853de2c98fb7e8489dc
Manikandan Sundararajan <mani@hey.com>
2021-12-23 04:26:22 -0500

    use the mutation method in evolve

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,7 +1,7 @@
 // #![feature(crate_visibility_modifier)]
 // #![feature(min_type_alias_impl_trait)]
 
-pub use self::{chromosome::*, crossover::*, individual::*, selection::*};
+pub use self::{chromosome::*, crossover::*, individual::*, mutation::*, selection::*};
 
 use rand::seq::SliceRandom;
 use rand::{Rng, RngCore};
@@ -9,21 +9,28 @@ use rand::{Rng, RngCore};
 mod chromosome;
 mod crossover;
 mod individual;
+mod mutation;
 mod selection;
 
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
     crossover_method: Box<dyn CrossoverMethod>,
+    mutation_method: Box<dyn MutationMethod>,
 }
 
 impl<S> GeneticAlgorithm<S>
 where
     S: SelectionMethod,
 {
-    pub fn new(selection_method: S, crossover_method: impl CrossoverMethod + 'static) -> Self {
+    pub fn new(
+        selection_method: S,
+        crossover_method: impl CrossoverMethod + 'static,
+        mutation_method: impl MutationMethod + 'static,
+    ) -> Self {
         Self {
             selection_method,
             crossover_method: Box::new(crossover_method),
+            mutation_method: Box::new(mutation_method),
         }
     }
 
@@ -42,8 +49,8 @@ where
 
                 let mut child = self.crossover_method.crossover(rng, parent_a, parent_b);
 
-                // TODO crossover
-                // TODO mutation
+                self.mutation_method.mutate(rng, &mut child);
+
                 // TODO convert `Chromosome` back into `Individual`
                 todo!()
             })
