593bc38004e049f78a1f6d68d04bc7daf0ad0932
Manikandan Sundararajan <mani@hey.com>
2021-12-23 06:48:04 -0500

    output the world we've created to the console

--- www/index.js
+++ www/index.js
@@ -1,3 +1,6 @@
 import * as sim from 'lib-simulation-wasm';
 
-alert("Who's that pokemon?: " + sim.whos_that_pokemon() + '!');
+const simulation = new sim.Simulation();
+const world = simulation.world();
+
+console.log(world);
