687b838eed2d7285c6ab632985c661c84b5ae2a5
Manikandan Sundararajan <mani@hey.com>
2021-12-23 07:08:50 -0500

    function to draw a triangle

--- www/index.js
+++ www/index.js
@@ -1,5 +1,16 @@
 import * as sim from 'lib-simulation-wasm';
 
+CanvasRenderingContext2D.prototype.drawTriangle = function (x, y, size) {
+	this.beginPath();
+	this.moveTo(x, y);
+	this.lineTo(x + size, y + size);
+	this.lineTo(x - size, y + size);
+	this.lineTo(x, y);
+
+	this.fillStyle = 'rgb(0, 0, 0)';
+	this.fill();
+};
+
 const simulation = new sim.Simulation();
 const viewport = document.getElementById('viewport');
 const viewportWidth = viewport.width;
@@ -19,5 +30,9 @@ ctxt.scale(viewportScale, viewportScale);
 ctxt.fillStyle = 'rgb(0, 0, 0)';
 
 for (const animal of simulation.world().animals) {
-	ctxt.fillRect(animal.x * viewportWidth, animal.y * viewportHeight, 15, 15);
+	ctxt.drawTriangle(
+		animal.x * viewportWidth,
+		animal.y * viewportHeight,
+		0.01 * viewportWidth
+	);
 }
