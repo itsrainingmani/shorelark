aa77a4b9672e487ac0a89e4e6e3b34cb2d09564e
Manikandan Sundararajan <mani@hey.com>
2021-12-23 06:24:52 -0500

    impl random fn and some getters

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,4 +1,5 @@
 use nalgebra as na;
+use rand::{Rng, RngCore};
 
 pub struct Simulation {
     world: World,
@@ -20,4 +21,65 @@ pub struct Animal {
 #[derive(Debug)]
 pub struct Food {
     position: na::Point2<f32>,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+}
+
+impl World {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        let animals = (0..40).map(|_| Animal::random(rng)).collect();
+        let foods = (0..60).map(|_| Food::random(rng)).collect();
+
+        // ^ current impl allows animals and foods to overlap which isn't ideal
+
+        Self { animals, foods }
+    }
+
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    pub fn foods(&self) -> &[Food] {
+        &self.foods
+    }
+}
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+            rotation: rng.gen(),
+            speed: 0.002,
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+
+    pub fn rotation(&self) -> na::Point2<f32> {
+        self.rotation
+    }
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
 }
