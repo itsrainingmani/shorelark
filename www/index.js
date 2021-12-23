625bf81ffb69b60eb696abc1527cc188913970d8
Manikandan Sundararajan <mani@hey.com>
2021-12-23 07:15:57 -0500

    do some trig to rotate the triangle when drawing

--- www/index.js
+++ www/index.js
@@ -1,14 +1,26 @@
 import * as sim from 'lib-simulation-wasm';
 
-CanvasRenderingContext2D.prototype.drawTriangle = function (x, y, size) {
+CanvasRenderingContext2D.prototype.drawTriangle = function (
+	x,
+	y,
+	size,
+	rotation
+) {
 	this.beginPath();
-	this.moveTo(x, y);
-	this.lineTo(x + size, y + size);
-	this.lineTo(x - size, y + size);
+	this.moveTo(x + Math.cos(rotation) * 1.5, y + Math.sin(rotation) * 1.5);
+
+	this.lineTo(
+		x + Math.cos(rotation + (2.0 / 3.0) * Math.PI) * size,
+		y + Math.sin(rotation + (2.0 / 3.0) * Math.PI) * size
+	);
+	this.lineTo(
+		x + Math.cos(rotation - (2.0 / 3.0) * Math.PI) * size,
+		y + Math.sin(rotation - (2.0 / 3.0) * Math.PI) * size
+	);
 	this.lineTo(x, y);
 
-	this.fillStyle = 'rgb(0, 0, 0)';
-	this.fill();
+	this.lineTo(x + Math.cos(rotation) * 1.5, y + Math.sin(rotation) * 1.5);
+	this.stroke();
 };
 
 const simulation = new sim.Simulation();
@@ -33,6 +45,7 @@ for (const animal of simulation.world().animals) {
 	ctxt.drawTriangle(
 		animal.x * viewportWidth,
 		animal.y * viewportHeight,
-		0.01 * viewportWidth
+		0.025 * viewportWidth,
+		animal.rotation
 	);
 }
