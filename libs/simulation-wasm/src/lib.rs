8b57d98156368ae840d7e3c6a8bdc45fdfdf2a68
Manikandan Sundararajan <mani@hey.com>
2021-12-23 06:32:33 -0500

    build a sample simulation for wasm

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,4 +1,23 @@
+use lib_simulation as sim;
+use rand::prelude::*;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
+}
 
 #[wasm_bindgen]
 pub fn whos_that_pokemon() -> String {
